4eae5b7b31199c689fcf2820d76e1b14398bfcc0
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-10-18 00:23:05 +0700

    new comps app

---
+++ comps/src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button";
+function App(){
+    return <div>
+        <div><Button></Button></div>
+        <div><Button></Button></div>
+        <div><Button></Button></div>
+        <div><Button></Button></div>
+        <div><Button></Button></div>
+    </div>;
+}
+export default App;
