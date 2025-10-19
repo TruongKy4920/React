07694163becc341fd1e0734e1897af6a21c8d760
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-10-20 00:28:23 +0700

    apply css tailwind rule

--- comps/src/Button.jsx
+++ comps/src/Button.jsx
@@ -1,4 +1,4 @@
 function Button({children,primary,secondary,success,warning,danger,outline,rounded}){
-    return <button>{children}</button>;
+    return <button className="px-3 py-1.5 bg-blue-700 text-white border border-green-500">{children}</button>;
 }
 export default Button;
