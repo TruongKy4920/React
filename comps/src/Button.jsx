182a650e1efaaaf34736eddd0389a26e11024350
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-10-19 23:56:17 +0700

    adding button props rules

--- comps/src/Button.jsx
+++ comps/src/Button.jsx
@@ -1,4 +1,4 @@
-function Button({children}){
+function Button({children,primary,secondary,success,warning,danger,outline,rounded}){
     return <button>{children}</button>;
 }
 export default Button;
