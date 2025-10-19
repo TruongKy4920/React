0117dbab05c204a41e5c1f98dee4f74a739d30e6
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-10-19 23:56:54 +0700

    adding button props rules2

--- comps/src/App.jsx
+++ comps/src/App.jsx
@@ -1,8 +1,8 @@
 import Button from "./Button";
 function App(){
     return <div>
-        <div><Button>Click there</Button></div>
-        <div><Button>Click here</Button></div>
+        <div><Button primary>Click there</Button></div>
+        <div><Button success>Click here</Button></div>
         <div><Button></Button></div>
         <div><Button></Button></div>
         <div><Button></Button></div>
