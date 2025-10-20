d736339cb01b6c5d04f648a0e0f2c4247070dfd5
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-10-20 14:15:36 +0700

    adding classname rules

--- comps/src/App.jsx
+++ comps/src/App.jsx
@@ -1,11 +1,11 @@
 import Button from "./Button";
 function App(){
     return <div>
-        <div><Button primary>Click there</Button></div>
-        <div><Button success>Click here</Button></div>
-        <div><Button>Click 1</Button></div>
-        <div><Button>Click 2</Button></div>
-        <div><Button>Click 3</Button></div>
+        <div><Button primary rounded>Click there</Button></div>
+        <div><Button success outline>Click here</Button></div>
+        <div><Button secondary rounded>Click 1</Button></div>
+        <div><Button danger outline>Click 2</Button></div>
+        <div><Button warning rounded>Click 3</Button></div>
     </div>;
 }
 export default App;
--- comps/src/Button.jsx
+++ comps/src/Button.jsx
@@ -8,7 +8,13 @@ function Button({children,
     outline,
     rounded}){
     const classes= className('px-3 py-1.5 border',{
-        "bg-blue-700 text-white": primary
+        "bg-blue-700 text-white": primary, 
+        "bg-black text-white":secondary,
+        "bg-green-500 ":success,
+        "bg-yellow-400":warning,
+        "bg-red-500":danger,
+        "rounded-full":rounded,
+        "border-4 border-pink-800":outline
     }); //có thể viết là className('px-3',' py-1.5',v.v..)
     //ở đây ta muốn thống nhất 1 form kích thước của các nút-> nút nào cũng px-3 và py1.5-> cái thứ đổi duy nhất là màu
     return <button className={classes}>{children}</button>;
