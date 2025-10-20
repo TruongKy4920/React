3cb03f46f6d560407f3c1982c96ff2fbd35231f0
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-10-20 14:53:40 +0700

    finishing button styling

--- comps/src/App.jsx
+++ comps/src/App.jsx
@@ -1,11 +1,11 @@
 import Button from "./Button";
 function App(){
     return <div>
-        <div><Button primary rounded>Click there</Button></div>
-        <div><Button success outline>Click here</Button></div>
-        <div><Button secondary rounded>Click 1</Button></div>
-        <div><Button danger outline>Click 2</Button></div>
-        <div><Button warning rounded>Click 3</Button></div>
+        <div><Button primary outline rounded>Click there</Button></div>
+        <div><Button success outline rounded>Click here</Button></div>
+        <div><Button secondary outline rounded>Click 1</Button></div>
+        <div><Button danger outline rounded>Click 2</Button></div>
+        <div><Button warning outline rounded>Click 3</Button></div>
     </div>;
 }
 export default App;
--- comps/src/Button.jsx
+++ comps/src/Button.jsx
@@ -1,4 +1,5 @@
 import className from 'classnames';
+import { twMerge } from 'tailwind-merge';
 function Button({children,
     primary,
     secondary,
@@ -10,13 +11,30 @@ function Button({children,
     const classes= className('px-3 py-1.5 border',{
         "bg-blue-700 text-white": primary, 
         "bg-black text-white":secondary,
-        "bg-green-500 ":success,
-        "bg-yellow-400":warning,
-        "bg-red-500":danger,
-        "rounded-full":rounded,
-        "border-4 border-pink-800":outline
+        "bg-green-600 text-white ":success,
+        "bg-yellow-400 text-white":warning,
+        "bg-red-600 text-white":danger,
+        "rounded-full ":rounded,
+        "border-2 border-blue-600 text-blue-600 bg-white":outline && primary
     }); //có thể viết là className('px-3',' py-1.5',v.v..)
     //ở đây ta muốn thống nhất 1 form kích thước của các nút-> nút nào cũng px-3 và py1.5-> cái thứ đổi duy nhất là màu
-    return <button className={classes}>{children}</button>;
+    //tao classes 2 là vì text white sẽ ghì đè mọi màu text khác-> sử dụng thư viện merge tailwind để không bị nữa
+     const classes2= twMerge(className('px-3 py-1.5 border',{
+        "border-2 border-blue-600 bg-blue-700 text-white": primary, 
+        "border-2 border-black bg-black text-white":secondary,
+        "border-2 border-green-600 bg-green-600 text-white ":success,
+        "border-2 border-yellow-600 bg-yellow-400 text-white":warning,
+        "border-2 border-red-600 bg-red-600 text-white":danger,
+        "rounded-full ":rounded,
+        "bg-white": outline,
+        "text-blue-600":outline && primary,
+        "text-red-600 ":outline && danger,
+        " text-yellow-600 ":outline && warning,
+        "text-black ":outline && secondary,
+        "text-green-600":outline && success,
+        
+    })
+);
+    return <button className={classes2}>{children}</button>;
 }
 export default Button;
