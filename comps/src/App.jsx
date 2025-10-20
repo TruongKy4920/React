75d56255df1fbe9cd04e509e5900e16b96172a6f
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-10-20 18:08:28 +0700

    adding icons

--- comps/src/App.jsx
+++ comps/src/App.jsx
@@ -1,10 +1,17 @@
 import Button from "./Button";
+import {GoBell,GoBook,GoBeaker} from 'react-icons/go';
 function App(){
     return <div>
-        <div><Button primary outline rounded>Click there</Button></div>
-        <div><Button success outline rounded>Click here</Button></div>
-        <div><Button secondary outline rounded>Click 1</Button></div>
-        <div><Button danger outline rounded>Click 2</Button></div>
+        <div><Button primary outline rounded> 
+                 <GoBell></GoBell> Click there
+            </Button>
+        </div>
+        <div><Button success outline rounded>
+             <GoBook></GoBook> Click here
+            </Button>
+        </div>
+        <div><Button secondary outline rounded> <GoBeaker></GoBeaker> Click 1</Button></div>
+        <div><Button danger outline rounded> <GoBook></GoBook> Click 2222</Button></div>
         <div><Button warning outline rounded>Click 3</Button></div>
     </div>;
 }
--- comps/src/Button.jsx
+++ comps/src/Button.jsx
@@ -1,5 +1,6 @@
 import className from 'classnames';
 import { twMerge } from 'tailwind-merge';
+
 function Button({children,
     primary,
     secondary,
@@ -19,7 +20,7 @@ function Button({children,
     }); //có thể viết là className('px-3',' py-1.5',v.v..)
     //ở đây ta muốn thống nhất 1 form kích thước của các nút-> nút nào cũng px-3 và py1.5-> cái thứ đổi duy nhất là màu
     //tao classes 2 là vì text white sẽ ghì đè mọi màu text khác-> sử dụng thư viện merge tailwind để không bị nữa
-     const classes2= twMerge(className('px-3 py-1.5 border',{
+     const classes2= twMerge(className('flex items-center px-3 py-1.5 border',{
         "border-2 border-blue-600 bg-blue-700 text-white": primary, 
         "border-2 border-black bg-black text-white":secondary,
         "border-2 border-green-600 bg-green-600 text-white ":success,
